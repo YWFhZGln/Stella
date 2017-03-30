ab5df0426d89e0bc96db95dd1f9c6f9bf4ac5125
ishu <iggarg999@gmail.com>
2017-03-30 23:43:51 +0530

    Dashboard added

--- app/src/main/java/com/higgsontech/stella/Dashboard.java
+++ app/src/main/java/com/higgsontech/stella/Dashboard.java
@@ -1,8 +1,18 @@
 package com.higgsontech.stella;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
 /**
  * Created by ishu on 30/3/17.
  */
 
-public class Dashboard {
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
 }
