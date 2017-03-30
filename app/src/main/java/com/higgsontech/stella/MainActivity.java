68a5c17255d18ff65018f67ef33a0b150b7e3c02
iamayushman <ayushmanabc@gmail.com>
2017-03-30 16:43:17 +0530

    ayushman commit

--- app/src/main/java/com/higgsontech/stella/MainActivity.java
+++ app/src/main/java/com/higgsontech/stella/MainActivity.java
@@ -2,6 +2,7 @@ package com.higgsontech.stella;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
