380b711cae9e312ae88421ad36c78bf0479c3fb3
YWFhZGln <ayu@rpiit.com>
2017-04-01 15:15:52 +0530

    Schedule Preference implementation

--- app/src/main/java/com/higgsontech/stella/Constants.java
+++ app/src/main/java/com/higgsontech/stella/Constants.java
@@ -7,10 +7,27 @@ import android.content.SharedPreferences;
  */
 
 public class Constants {
-    public  static final String Login = "Login";
-    public static final String MyPREFERENCES = "MyPREFERENCES";
+    //public  static final String Login = "Login";
+    //public static final String MyPREFERENCES = "MyPREFERENCES";
     public static final String Email = "email" ;
     public static final String Passwd = "password" ;
     public static final String loginResponse = "lResponse";
-    public  static SharedPreferences sharedpreferences = null;
+    public static final String id = "id";
+    public static final String fname = "fname";
+    public static final String lname = "lname";
+    public static final String designation = "designation";
+
+    private static SharedPreferences sharedpreferences = null;
+
+    public static void setSharedpreferences(SharedPreferences preferences){
+        sharedpreferences = preferences;
+    }
+    public static void  updateSharedPrefence(String key, String value){
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+    public static String fetchSharedPreferenceValues(String key){
+        return sharedpreferences.getString(key,"NULL");
+    }
 }
--- app/src/main/java/com/higgsontech/stella/Login.java
+++ app/src/main/java/com/higgsontech/stella/Login.java
@@ -4,29 +4,45 @@ package com.higgsontech.stella;
  * Created by cloud on 30/3/17.
  */
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-/**
- * Created by cloud on 30/3/17.
- */
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static com.higgsontech.stella.R.layout.payment_status;
 
 public class Login extends AppCompatActivity {
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    EditText et_fname,et_lname,et_aadhar,et_office_pin,et_home_pin,et_signup_email,et_signup_password,et_mobile, et_email,et_password;
-    String u_fname,u_lname,u_gender,u_aadhar,u_designation,u_office_pin,u_home_pin,u_signup_email,u_signup_password,u_mobile,u_email,u_password;
+    EditText et_fname, et_lname, et_aadhar, et_office_pin, et_home_pin, et_signup_email, et_signup_password, et_mobile, et_email, et_password;
+    String u_fname, u_lname, u_gender, u_aadhar, u_designation, u_office_pin, u_home_pin, u_signup_email, u_signup_password, u_mobile, u_email, u_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
 
 
     }
@@ -49,49 +65,60 @@ public class Login extends AppCompatActivity {
     }
 
 
-    public void register(View view)
-    {
-        et_fname               =   (EditText) findViewById(R.id.fname);
-        et_lname               =   (EditText) findViewById(R.id.lname);
-        et_aadhar              =   (EditText) findViewById(R.id.aadhar);
-        et_office_pin          =   (EditText) findViewById(R.id.office_pin);
-        et_home_pin            =   (EditText) findViewById(R.id.home_pin);
-        et_signup_email        =   (EditText) findViewById(R.id.signup_email);
-        et_signup_password     =   (EditText) findViewById(R.id.signup_password);
-        et_mobile              =    (EditText) findViewById(R.id.mobile);
+    public void register(View view) {
+        et_fname = (EditText) findViewById(R.id.fname);
+        et_lname = (EditText) findViewById(R.id.lname);
+        et_aadhar = (EditText) findViewById(R.id.aadhar);
+        et_office_pin = (EditText) findViewById(R.id.office_pin);
+        et_home_pin = (EditText) findViewById(R.id.home_pin);
+        et_signup_email = (EditText) findViewById(R.id.signup_email);
+        et_signup_password = (EditText) findViewById(R.id.signup_password);
+        et_mobile = (EditText) findViewById(R.id.mobile);
 
-        u_fname                   =   et_fname.getText().toString();
-        u_lname                   =   et_lname.getText().toString();
-        u_gender                  =   "Male";
-        u_aadhar                  =   et_aadhar.getText().toString();
-        u_designation             =   "Center Head";
-        u_office_pin              =   et_office_pin.getText().toString();
-        u_home_pin                =   et_home_pin.getText().toString();
-        u_signup_email            =   et_signup_email.getText().toString();
-        u_signup_password         =   et_signup_password.getText().toString();
-        u_mobile                  =   et_mobile.getText().toString();
+        u_fname = et_fname.getText().toString();
+        u_lname = et_lname.getText().toString();
+        u_gender = "Male";
+        u_aadhar = et_aadhar.getText().toString();
+        u_designation = "Center Head";
+        u_office_pin = et_office_pin.getText().toString();
+        u_home_pin = et_home_pin.getText().toString();
+        u_signup_email = et_signup_email.getText().toString();
+        u_signup_password = et_signup_password.getText().toString();
+        u_mobile = et_mobile.getText().toString();
 
-        String method   =   "register";
+        String method = "register";
 
-        BackgroundTask backgroundTask=new BackgroundTask(this);
-        backgroundTask.execute(method,u_fname,u_lname,u_gender,u_aadhar,u_designation,u_office_pin,u_home_pin,u_signup_email,u_signup_password,u_mobile);
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, u_fname, u_lname, u_gender, u_aadhar, u_designation, u_office_pin, u_home_pin, u_signup_email, u_signup_password, u_mobile);
 
     }
 
 
-    public void login(View view)
-    {
-        et_email        =   (EditText) findViewById(R.id.email);
-        et_password     =   (EditText) findViewById(R.id.password);
-        u_email         =   et_email.getText().toString();
-        u_password      =   et_password.getText().toString();
-        String method   =   "login";
+    public void login(View view) {
+        et_email = (EditText) findViewById(R.id.email);
+        et_password = (EditText) findViewById(R.id.password);
+        u_email = et_email.getText().toString();
+        u_password = et_password.getText().toString();
+        String method = "login";
 
-        Constants.sharedpreferences = getSharedPreferences("MyPref", 0);
-        BackgroundTask backgroundTask=new BackgroundTask(this);
-        backgroundTask.execute(method,u_email,u_password);
-        
+        Constants.setSharedpreferences(getSharedPreferences("MyPref", 0));
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, u_email, u_password);
+        try {
+            int designation_array = R.array.designation_array;
+            switch (Constants.fetchSharedPreferenceValues(Constants.designation)) {
+                case "Center Head":
+                    LayoutInflater.from(getApplicationContext()).inflate(R.layout.center_list, null, false);
+                    break;
+                case "Exam Center Head":
+                    //TODO:
+
+                    break;
+                default:
+                    LayoutInflater.from(getApplicationContext()).inflate(R.layout.center_list, null, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
