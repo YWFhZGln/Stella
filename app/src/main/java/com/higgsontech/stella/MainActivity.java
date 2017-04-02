d3ea5e9cde5de1013a5e78e0cf2824ba824de5ba
Aman Hanjra <amanhanjra95@gmail.com>
2017-04-02 09:33:26 +0530

    added splash

--- app/src/main/java/com/higgsontech/stella/MainActivity.java
+++ app/src/main/java/com/higgsontech/stella/MainActivity.java
@@ -4,7 +4,7 @@ package com.higgsontech.stella;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
@@ -13,10 +13,19 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },1000);
+
     }
 
-    public void gotoLoginActivity(View view) {
-        Intent i = new Intent(this, Login.class);
-        startActivity(i);
-    }
+
 }
